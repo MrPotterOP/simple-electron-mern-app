cd5bd900981ed600156ff7c2cb069f0670017083
Shubham Ubarhande <shubham.ubarhande1044@gmail.com>
2022-11-28 12:33:00 +0530

    protected homepage route added to api

--- backend/router/routes.js
+++ backend/router/routes.js
@@ -3,6 +3,10 @@ import express from "express";
 //Controllers
 import postRegister from "../controllers/postRegister.js";
 import postLogin from "../controllers/postLogin.js";
+import getHomePage from "../controllers/getHomePage.js";
+
+//Middlewares
+import checkToken from "../middlewares/checkToken.js";
 
 const router = express.Router();
 
@@ -10,6 +14,7 @@ const router = express.Router();
 
 router.post("/register", postRegister);
 router.post("/login", postLogin);
+router.get("/homepage", checkToken, getHomePage);
 
 
 export default router;
